bd26a7aded8b4c36490e1116972067e02e0a95a4
Franco Testori <franco.testori@ing.austral.edu.ar>
2015-02-05 16:46:13 -0300

    cleaned-up controllers.Users

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,34 +1,14 @@
 package controllers;
 
-import models.User;
-import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
-
-import static play.data.Form.form;
 
 /**
  * Created by nicolas on 22/01/15.
  */
 public class Users extends Controller{
 
-    public static Result showRegisterForm(){
-        return ok(registerForm.render());
-    }
-
-    public static Result register() {
-        Form<User> userForm = form(User.class).bindFromRequest();
-        if (userForm.hasErrors()){
-            return badRequest(registerForm.render());
-        } else {
-            User user = userForm.get();
-            user.save();
-            session().clear();
-            session("email", user.getEmail());
-            return redirect(controllers.routes.Application.index());
-        }
-    }
-
     public static Result showProfile(){return ok(profile.render());}
+
 }
