99408401a1e6534299b7bb3134e17fcd56087b7c
Franco Testori <franco.testori@ing.austral.edu.ar>
2015-02-05 16:34:40 -0300

    Added profile, and CRUD project views

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -55,20 +55,4 @@ public class Application extends Controller {
         }
     }
 
-    public static Result showRegisterForm(){
-        return ok(registerForm.render());
-    }
-
-    public static Result register() {
-        Form<User> userForm = form(User.class).bindFromRequest();
-        if (userForm.hasErrors()){
-            return badRequest(registerForm.render());
-        } else {
-            User user = userForm.get();
-            user.save();
-            session().clear();
-            session("email", user.getEmail());
-            return redirect(controllers.routes.Application.index());
-        }
-    }
 }
--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -13,4 +13,22 @@ import static play.data.Form.form;
  */
 public class Users extends Controller{
 
+    public static Result showRegisterForm(){
+        return ok(registerForm.render());
+    }
+
+    public static Result register() {
+        Form<User> userForm = form(User.class).bindFromRequest();
+        if (userForm.hasErrors()){
+            return badRequest(registerForm.render());
+        } else {
+            User user = userForm.get();
+            user.save();
+            session().clear();
+            session("email", user.getEmail());
+            return redirect(controllers.routes.Application.index());
+        }
+    }
+
+    public static Result showProfile(){return ok(profile.render());}
 }
